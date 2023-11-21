50398874f8cf063797004c88e4bafa70fa89ef56
aetrad <abde@live.be>
2023-11-21 18:05:02 +0100

    Ajout du fichier classobjet.cs et modifications apportées

---
+++ classchest.cs
@@ -0,0 +1,56 @@
+public class Chest
+{
+    // La méthode Open pourrait être implémentée ici.
+    public virtual void Open()
+    {
+        // Logique d'ouverture du coffre commun
+    }
+}
+
+public class PotionChest : Chest
+{
+    public int Force { get; set; }
+    public List<Potion> ListPotion { get; set; }
+
+    public override void Open()
+    {
+        base.Open();
+        // Logique spécifique à l'ouverture d'un coffre à potions
+    }
+}
+
+public class XPChest : Chest
+{
+    public string AfterFight { get; set; }
+    public List<int> ListXPAfterFight { get; set; }
+
+    public override void Open()
+    {
+        base.Open();
+        // Logique spécifique à l'ouverture d'un coffre à XP
+    }
+}
+
+public class WeaponChest : Chest
+{
+    public int WeaponForce { get; set; }
+    public List<Weapon> ListWeaponRoom { get; set; }
+
+    public override void Open()
+    {
+        base.Open();
+        // Logique spécifique à l'ouverture d'un coffre d'armes
+    }
+}
+
+// Classes supplémentaires pour représenter des potions et des armes, 
+// pour lesquelles nous n'avons pas de détails dans le diagramme.
+public class Potion
+{
+    // Propriétés de la potion ici
+}
+
+public class Weapon
+{
+    // Propriétés de l'arme ici
+}
